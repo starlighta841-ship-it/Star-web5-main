de4300f9faa78091bed8b9187663eacde4bd8fde
starlighta841-ship-it <starlighta841@gmail.com>
2026-05-04 00:39:24 +0600

    Update script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const showBtn = document.getElementById('showNav__btn')
+const headerNav = document.querySelector('.header-nav')
+const hideBtn = document.getElementById('hideNav__btn')
+
+function hideNav(){
+    headerNav.style.display = 'none';
+}
+function showNav(){
+    headerNav.style.display = 'block';
+}
